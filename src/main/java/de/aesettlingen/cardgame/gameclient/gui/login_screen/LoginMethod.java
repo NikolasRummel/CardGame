fc1c9d014409fb8ba07447d9ba0e025fb9f2d423
pxxx <p_xxx@tutanota.com>
2022-06-30 15:50:09 +0200

    add login screen

---
+++ src/main/java/de/aesettlingen/cardgame/gameclient/gui/login_screen/LoginMethod.java
@@ -0,0 +1,5 @@
+package de.aesettlingen.cardgame.gameclient.gui.login_screen;
+
+public interface LoginMethod {
+    void login(String loginName);
+}
---
+++ src/main/java/de/aesettlingen/cardgame/gameclient/gui/login_screen/LoginScreen.java
@@ -0,0 +1,41 @@
+package de.aesettlingen.cardgame.gameclient.gui.login_screen;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class LoginScreen extends JPanel {
+
+    private JButton loginButton;
+    private JLabel textForButton = new JLabel("login name");
+    private JTextField inputField = new JTextField();
+
+    private LoginMethod loginMethod;
+
+    public LoginScreen(LoginMethod loginMethod) {
+        this.loginMethod = loginMethod;
+        initGuiElements();
+    }
+
+    private void initGuiElements() {
+        loginButton = new JButton("login");
+        loginButton.addActionListener(al -> loginMethod.login(inputField.getText()));
+
+        inputField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) loginMethod.login(inputField.getText());
+            }
+        });
+
+        var panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.add(textForButton, LEFT_ALIGNMENT);
+        panel.add(loginButton);
+
+        this.setLayout(new BorderLayout());
+        this.add(panel, BorderLayout.CENTER);
+        this.add(loginButton, BorderLayout.SOUTH);
+    }
+}
