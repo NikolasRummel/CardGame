9816952f8ceef16d6340f001ea73813978f61002
pxxx <p_xxx@tutanota.com>
2022-07-01 18:06:37 +0200

    finished implementation of card

--- src/main/java/de/aesettlingen/cardgame/logic/Card.java
+++ src/main/java/de/aesettlingen/cardgame/logic/Card.java
@@ -1,13 +1,19 @@
 package de.aesettlingen.cardgame.logic;
 
 public class Card {
-    public void Card(String name, String color, int value) {
-
-    }
-
     private String name;
     private String color;
     private int value;
+    public void Card(String name, String color, int value) {
+        this.name = name;
+        this.color = color;
+        this.value = value;
+    }
+
+    public void Card(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
 
     public String getName() {
         return name;
