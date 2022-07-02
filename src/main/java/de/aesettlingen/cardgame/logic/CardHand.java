71f3a47b449496938c186aedba23b9fe06dd62aa
pxxx <p_xxx@tutanota.com>
2022-07-02 13:16:47 +0200

    implemented CardHand

--- src/main/java/de/aesettlingen/cardgame/logic/CardHand.java
+++ src/main/java/de/aesettlingen/cardgame/logic/CardHand.java
@@ -1,17 +1,44 @@
 package de.aesettlingen.cardgame.logic;
 
-public class CardHand {
-    private Card[] cards = new Card[0];
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
 
-    public Card[] getCards() {
-        return new Card[0];
+public class CardHand extends CardCollection {
+
+    CardHand() {
+        super();
+    }
+
+    CardHand(LinkedList<Card> cards) {
+        super(cards);
+    }
+
+    CardHand(Card ... cards) {
+        super(cards);
     }
 
     public int getCombinedValue() {
-        return 0;
+        AtomicInteger out = new AtomicInteger();
+        cards.forEach((Card it) -> out.addAndGet(it.getValue()));
+        return out.get();
     }
 
     public Card pullRandom() {
-        return null;
+        return cards.remove((int) (Math.random()*cards.size()));
+    }
+
+    public LinkedList<Card> pullRandom(int number) {
+        LinkedList<Card> out = new LinkedList<>();
+        for (int i = 0; i < number; i++)
+            out.add(pullRandom());
+        return out;
+    }
+
+    public void add(Card card) {
+        super.cards.add(card);
+    }
+
+    public void add(LinkedList<Card> card) {
+        super.cards.addAll(card);
     }
 }
