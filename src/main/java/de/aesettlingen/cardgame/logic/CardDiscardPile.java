093f7310cbc48e964621bf2db1da8278ef78ff9f
pxxx <p_xxx@tutanota.com>
2022-07-02 13:56:57 +0200

    remove function legeKarteAb because it will later inherent it from card steck

--- src/main/java/de/aesettlingen/cardgame/logic/CardDiscardPile.java
+++ src/main/java/de/aesettlingen/cardgame/logic/CardDiscardPile.java
@@ -1,7 +1,4 @@
 package de.aesettlingen.cardgame.logic;
 
 public class CardDiscardPile {
-    private void legeKarteAb(){         //TODO Karte in Klammer keine Ahnung was für ein Datentyp etc
-
-    }
 }
