de20eee786c5efd013e862291fe150096f534902
Nikolas Rummel <skyzzytv1@gmail.com>
2022-07-06 23:01:31 +0200

    Implemented reconnection to server

--- src/main/java/de/aesettlingen/cardgame/commons/networking/listener/ClientPacketListener.java
+++ src/main/java/de/aesettlingen/cardgame/commons/networking/listener/ClientPacketListener.java
@@ -10,6 +10,8 @@ import de.aesettlingen.cardgame.commons.networking.packet.AbstractPacket;
 public class ClientPacketListener extends Thread {
 
     private final NetworkingClient client;
+    private int errorCount = 0;
+    private long lastError;
 
     public ClientPacketListener(NetworkingClient client) {
         this.client = client;
@@ -30,6 +32,22 @@ public class ClientPacketListener extends Thread {
 
             } catch (Exception e) {
                 System.out.println("Error with the connection to the server! Reason: " + e);
+                client.setRunning(false);
+                client.start(); //relogin
+                this.errorCount ++;
+                this.lastError = System.currentTimeMillis();
+
+                if(errorCount == 10) {
+
+                    //Reset if error is way back in time -> maybe just a short problem
+                    long delta = System.currentTimeMillis()-this.lastError;
+                    if(delta >= 10*1000) {
+                        this.errorCount = 1;
+                    }
+
+                    client.setRunning(false);
+                    System.err.println("WARNING: Tried 10 times to connect to server, but server is not reachable! ");
+                }
             }
         }
     }
