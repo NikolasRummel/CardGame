e5ed219faea94d9ccd1794a2fea740fbd8618652
pxxx <p_xxx@tutanota.com>
2022-07-08 17:20:39 +0200

    implement MauMauMove

--- src/main/java/de/aesettlingen/cardgame/logic/MauMauMove.java
+++ src/main/java/de/aesettlingen/cardgame/logic/MauMauMove.java
@@ -1,2 +1,16 @@
-package de.aesettlingen.cardgame.logic;public class MauMauMove {
+package de.aesettlingen.cardgame.logic;
+
+import de.aesettlingen.cardgame.logic.card.Card;
+
+public class MauMauMove extends Move {
+    private final Card card;
+
+    public MauMauMove(String playerName, Card card) {
+        super(playerName);
+        this.card = card;
+    }
+
+    public Card getCard() {
+            return this.card;
+    }
 }
