b66f62ae982f59bb7da9d8b34d65b6e88bd7aabc
tete77 <gw4tete,git>
2022-07-08 10:51:23 +0200

    Gamelogic fertig

--- src/main/java/de/aesettlingen/cardgame/logic/card/CardDeck.java
+++
@@ -1,9 +0,0 @@
-package de.aesettlingen.cardgame.logic.card;
-// TODO: implement/change according to the class diagram
-public class CardDeck {
-    private Card[] cards = new Card[0]; // TODO: remove because this field is implemented through extending CardCollection
-
-    public Card[] getCards() { // TODO: remove because this function is implemented through extending CardCollection
-        return cards;
-    }
-}
