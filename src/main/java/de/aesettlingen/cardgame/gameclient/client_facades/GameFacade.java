6ca501e14ee6ccee4672b1614137c02ebedc80b6
pxxx <p_xxx@tutanota.com>
2022-07-09 23:09:52 +0200

    removed redundant accessors

--- src/main/java/de/aesettlingen/cardgame/gameclient/client_facades/GameFacade.java
+++ src/main/java/de/aesettlingen/cardgame/gameclient/client_facades/GameFacade.java
@@ -1,7 +1,5 @@
 package de.aesettlingen.cardgame.gameclient.client_facades;
 
-import de.aesettlingen.cardgame.logic.Player;
-
 interface GameFacade {
 	String getNameOfPlayer();
 }
--- src/main/java/de/aesettlingen/cardgame/gameclient/client_facades/MauMauFacade.java
+++ src/main/java/de/aesettlingen/cardgame/gameclient/client_facades/MauMauFacade.java
@@ -3,6 +3,5 @@ package de.aesettlingen.cardgame.gameclient.client_facades;
 import de.aesettlingen.cardgame.logic.card.Card;
 
 public abstract class MauMauFacade implements GameFacade {
-
     public abstract void sendCardOfMove(Card card);
 }
