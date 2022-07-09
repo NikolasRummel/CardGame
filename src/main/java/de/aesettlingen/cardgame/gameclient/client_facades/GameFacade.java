057d7e78fe7e999d30a11c9bef4f7f49f93532de
pxxx <p_xxx@tutanota.com>
2022-07-09 19:41:01 +0200

    remove redundant method

--- src/main/java/de/aesettlingen/cardgame/gameclient/client_facades/GameFacade.java
+++ src/main/java/de/aesettlingen/cardgame/gameclient/client_facades/GameFacade.java
@@ -2,12 +2,14 @@ package de.aesettlingen.cardgame.gameclient.client_facades;
 
 import de.aesettlingen.cardgame.gameclient.gui.game_gui.card_panel.SelectCardMethod;
 import de.aesettlingen.cardgame.logic.card.Card;
+import de.aesettlingen.cardgame.logic.mau_mau.MauMauMove;
 import de.aesettlingen.cardgame.logic.mau_mau.MauMauPlayer;
 
 public abstract class GameFacade {
 
-    public abstract SelectCardMethod getSelectCardMethod();
     public abstract MauMauPlayer getPlayer();
 
     public abstract Card getTopCard();
+
+    public abstract MauMauMove sendCardOfMove(Card Move);
 }
