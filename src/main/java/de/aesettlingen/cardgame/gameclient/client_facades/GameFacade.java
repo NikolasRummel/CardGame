46b8788c9b6ac988497ac87041379c053c21bdcb
pxxx <p_xxx@tutanota.com>
2022-07-09 16:25:33 +0200

    add get top card method

--- src/main/java/de/aesettlingen/cardgame/gameclient/client_facades/GameFacade.java
+++ src/main/java/de/aesettlingen/cardgame/gameclient/client_facades/GameFacade.java
@@ -1,10 +1,13 @@
 package de.aesettlingen.cardgame.gameclient.client_facades;
 
 import de.aesettlingen.cardgame.gameclient.gui.game_gui.card_panel.SelectCardMethod;
+import de.aesettlingen.cardgame.logic.card.Card;
 import de.aesettlingen.cardgame.logic.mau_mau.MauMauPlayer;
 
 public abstract class GameFacade {
 
     public abstract SelectCardMethod getSelectCardMethod();
     public abstract MauMauPlayer getPlayer();
+
+    public abstract Card getTopCard();
 }
