7b0619dda13f664522463a3220c214219ad50173
pxxx <p_xxx@tutanota.com>
2022-07-09 22:26:12 +0200

    just give accass to the player name in stead of the Player Object

--- src/main/java/de/aesettlingen/cardgame/gameclient/client_facades/GameFacade.java
+++ src/main/java/de/aesettlingen/cardgame/gameclient/client_facades/GameFacade.java
@@ -3,5 +3,5 @@ package de.aesettlingen.cardgame.gameclient.client_facades;
 import de.aesettlingen.cardgame.logic.Player;
 
 interface GameFacade {
-	Player getPlayer();
+	String getNameOfPlayer();
 }
